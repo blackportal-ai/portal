946b435171354d6c9a8e679c50163fb8e27309d7
Sviatoslav Makhynko <sviat.mak@gmail.com>
2024-12-13 21:39:21 +0100

    add: data

---
+++ src/data/features.ts
@@ -0,0 +1,23 @@
+import {
+  IoCogOutline,
+  IoConstructOutline,
+  IoHardwareChipOutline,
+} from 'react-icons/io5';
+
+export const features = [
+  {
+    icon: IoHardwareChipOutline,
+    title: 'Fast',
+    text: 'It is designed to be fast, with APIs that are easy to use while allowing advanced usage to customize and extend functionality.',
+  },
+  {
+    icon: IoConstructOutline,
+    title: 'Efficient and scalable tools',
+    text: 'It provides efficient and scalable tools for building and training neural networks.',
+  },
+  {
+    icon: IoCogOutline,
+    title: 'Multifunctional',
+    text: 'The framework aims to offer a comprehensive set of features, including various neural network layers, activation functions, loss functions, optimizers, and data handling utilities.',
+  },
+];
---
+++ src/data/social-links.ts
@@ -0,0 +1,14 @@
+import { SiGithub, SiX } from 'react-icons/si';
+
+export const socialLinks = [
+  {
+    icon: SiGithub,
+    href: 'https://github.com/delta-rs/delta',
+    label: 'GitHub',
+  },
+  {
+    icon: SiX,
+    href: 'https://x.com/deltaml_org',
+    label: 'X',
+  },
+];
