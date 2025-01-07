7bbe240a334414f536f36fe39811c2cdeb072fc6
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-01-07 15:56:01 +0100

    update: links

--- src/components/sections/home/Hero/index.tsx
+++ src/components/sections/home/Hero/index.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
                 </Button>
 
                 <Button variant="secondary" asChild>
-                  <Link href="https://github.com/delta-rs/delta">
+                  <Link href="https://github.com/blackportal-ai/website">
                     <SiGithub />
                     <span>Star 300+</span>
                   </Link>
@@ -57,7 +57,7 @@ export default function Hero() {
                 </Button>
 
                 <Button variant="secondary" asChild>
-                  <Link href="https://github.com/delta-rs/nebula">
+                  <Link href="https://github.com/blackportal-ai/nebula">
                     <SiGithub />
                     <span>Star 30+</span>
                   </Link>
--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -3,17 +3,17 @@ import { SiDiscord, SiGithub, SiX } from 'react-icons/si';
 export const socialLinks = [
   {
     icon: SiGithub,
-    href: 'https://github.com/delta-rs/delta',
+    href: 'https://github.com/blackportal-ai/delta',
     label: 'GitHub',
   },
   {
     icon: SiX,
-    href: 'https://x.com/deltaml_org',
+    href: 'https://x.com/blackportal_ai',
     label: 'X',
   },
   {
     icon: SiDiscord,
     href: 'https://discord.com/invite/S75dF9Pg',
-    label: 'Discord'
-  }
+    label: 'Discord',
+  },
 ];
