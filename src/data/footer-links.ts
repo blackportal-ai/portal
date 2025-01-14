c4c412d6260247de1918f8b7f40a247a0d335953
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-01-14 16:43:19 +0100

    add: footer links data

---
+++ src/data/footer-links.ts
@@ -0,0 +1,12 @@
+import type { MenuItem } from '@/types/menu';
+
+export const footerCompanyLinks: MenuItem[] = [
+  {
+    title: 'Terms of Conduct',
+    href: '/toc',
+  },
+  {
+    title: 'Privacy Policy',
+    href: '/privacy-policy',
+  },
+];
