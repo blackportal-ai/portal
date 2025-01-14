43fda250bc54da9e0773c2a1f3a863c4382aa223
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-01-14 17:12:27 +0100

    add: contact page to footer links

--- src/data/footer-links.ts
+++ src/data/footer-links.ts
@@ -2,6 +2,10 @@ import type { MenuItem } from '@/types/menu';
 
 export const footerCompanyLinks: MenuItem[] = [
   {
+    title: 'Contact Us',
+    href: '/contact',
+  },
+  {
     title: 'Terms of Conduct',
     href: '/toc',
   },
