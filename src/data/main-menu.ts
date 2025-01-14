678c88373055ddca615686a2b1b95c62446fa0f1
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-01-14 23:45:38 +0100

    add: about page to menu

--- src/data/main-menu.ts
+++ src/data/main-menu.ts
@@ -2,6 +2,10 @@ import type { MenuItem } from '@/types/menu';
 
 export const mainMenu: MenuItem[] = [
   {
+    title: 'About',
+    href: '/about',
+  },
+  {
     title: 'Contact Us',
     href: '/contact',
   },
