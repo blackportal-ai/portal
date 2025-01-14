adb6d29e7a9b20ec22e6e9a8437cb7b7b4801dd2
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-01-14 23:44:18 +0100

    add: about page data

---
+++ src/data/team.ts
@@ -0,0 +1,109 @@
+import { SiGithub, SiLinkedin, SiX } from 'react-icons/si';
+
+export const team = [
+  {
+    name: 'Marcus Cvjeticanin',
+    image: 'https://avatars.githubusercontent.com/u/33717111?v=4',
+    socialLinks: [
+      {
+        icon: SiX,
+        href: 'https://x.com/mjovanc',
+        label: 'X',
+      },
+      {
+        icon: SiLinkedin,
+        href: 'https://www.linkedin.com/in/marcuscvjeticanin/',
+        label: 'LinkedIn',
+      },
+      {
+        icon: SiGithub,
+        href: 'https://github.com/mjovanc',
+        label: 'GitHub',
+      },
+    ],
+  },
+  {
+    name: 'Chase Willden',
+    image: 'https://avatars.githubusercontent.com/u/3342754?v=4',
+    socialLinks: [
+      {
+        icon: SiX,
+        href: 'https://x.com/ChaseWillden',
+        label: 'X',
+      },
+      {
+        icon: SiLinkedin,
+        href: 'https://www.linkedin.com/in/chase-willden/',
+        label: 'LinkedIn',
+      },
+      {
+        icon: SiGithub,
+        href: 'https://github.com/chaseWillden',
+        label: 'GitHub',
+      },
+    ],
+  },
+  {
+    name: 'Tim Janus',
+    image: 'https://avatars.githubusercontent.com/u/5418311?v=4',
+    socialLinks: [
+      {
+        icon: SiX,
+        href: 'https://x.com/DarthB86',
+        label: 'X',
+      },
+      {
+        icon: SiLinkedin,
+        href: 'https://www.linkedin.com/in/tim-janus-07523b13/',
+        label: 'LinkedIn',
+      },
+      {
+        icon: SiGithub,
+        href: 'https://github.com/DarthB',
+        label: 'GitHub',
+      },
+    ],
+  },
+  {
+    name: 'Robin Hallabro-Kokko',
+    image: 'https://avatars.githubusercontent.com/u/44033026?v=4',
+    socialLinks: [
+      {
+        icon: SiX,
+        href: 'https://x.com/hallabrokokko',
+        label: 'X',
+      },
+      {
+        icon: SiLinkedin,
+        href: 'https://linkedin.com/in/robin-hallabro-kokko',
+        label: 'LinkedIn',
+      },
+      {
+        icon: SiGithub,
+        href: 'https://github.com/hallabro',
+        label: 'GitHub',
+      },
+    ],
+  },
+  {
+    name: 'Sviatoslav Makhynko',
+    image: 'https://avatars.githubusercontent.com/u/33163056?v=4',
+    socialLinks: [
+      {
+        icon: SiX,
+        href: 'https://x.com/SviatMak',
+        label: 'X',
+      },
+      {
+        icon: SiLinkedin,
+        href: 'https://www.linkedin.com/in/sviatoslav-makhynko-7ba465234/',
+        label: 'LinkedIn',
+      },
+      {
+        icon: SiGithub,
+        href: 'https://github.com/SvMak',
+        label: 'GitHub',
+      },
+    ],
+  },
+];
