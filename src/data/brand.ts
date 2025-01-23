784a37588157c260271d7434d6c7b068ef974fe4
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-01-23 20:57:09 +0100

    add: logos data

---
+++ src/data/brand.ts
@@ -0,0 +1,73 @@
+import type { BrandItem } from '@/types/brand';
+
+export const blackPortalLogos: BrandItem[] = [
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/blackportal/logo/blackportal_black_logo.svg',
+      },
+    },
+  },
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/blackportal/logo/blackportal_white_logo.svg',
+      },
+    },
+  },
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/blackportal/logo/blackportal_black_icon.svg',
+      },
+    },
+  },
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/blackportal/logo/blackportal_white_icon.svg',
+      },
+    },
+  },
+];
+
+export const deltaLogos: BrandItem[] = [
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/delta/logo/%CE%94_black_logo.svg',
+      },
+    },
+  },
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/delta/logo/%CE%94_white_logo.svg',
+      },
+    },
+  },
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/delta/logo/%CE%94_black_icon.svg',
+      },
+    },
+  },
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/delta/logo/%CE%94_white_icon.svg',
+      },
+    },
+  },
+];
+
+export const nebulaLogos: BrandItem[] = [
+  {
+    variants: {
+      svg: {
+        src: 'https://raw.githubusercontent.com/blackportal-ai/resources/13519fab43fc21cd189473955922f83c37927319/nebula/logo/nebula.svg',
+      },
+    },
+  },
+];
