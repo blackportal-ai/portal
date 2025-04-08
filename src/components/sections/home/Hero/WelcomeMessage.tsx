3e7990dbebd4ee1ba9818d36aa547b1fbab716c6
Marcus Cveticanin <k01522@norsk-tipping.no>
2025-04-08 21:14:23 +0200

    Updating descriptions

--- src/components/sections/home/Hero/WelcomeMessage.tsx
+++ src/components/sections/home/Hero/WelcomeMessage.tsx
@@ -1,7 +1,7 @@
 import { useTypewriter } from '@/hooks/use-typewriter';
 
 const text =
-  'A fast, open-source Rust solution for building scalable neural networks with easy-to-use, customizable APIs and comprehensive ML tools.';
+  'A fast, open-source Rust solution for building scalable machine learning models with easy-to-use, customizable APIs and comprehensive classical ML tools.';
 
 export default function WelcomeMessage() {
   const displayText = useTypewriter(text, 60);
