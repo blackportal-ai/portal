421f80da76b0b8d2b082440ca403f59dbf59846f
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-04-20 19:33:51 +0200

    add: horizon to hero

--- src/components/sections/home/Hero/index.tsx
+++ src/components/sections/home/Hero/index.tsx
@@ -10,17 +10,19 @@ import NebulaLogo from '@/components/icons/NebulaLogo';
 export default function Hero() {
   return (
     <section className="animate-in fade-in py-20 duration-500">
-      <div className="container mx-auto max-w-5xl">
-        <div className="animate-in slide-in-from-bottom-20 mb-12 duration-700">
+      <div className="container">
+        <div className="animate-in slide-in-from-bottom-20 mx-auto mb-12 max-w-5xl duration-700">
           <h1 className="text-center text-3xl font-bold md:text-4xl">
-            A fast, open-source Rust solution for building scalable machine learning models with easy-to-use, customizable APIs and comprehensive tools.
+            A fast, open-source Rust solution for building scalable machine
+            learning models with easy-to-use, customizable APIs and
+            comprehensive tools.
           </h1>
         </div>
 
-        <div className="animate-in slide-in-from-bottom-20 grid grid-cols-1 gap-6 duration-700 lg:grid-cols-2">
-          <Card className="relative overflow-hidden p-0 text-center">
-            <CardHeader className="hero-delta space-y-4 p-12">
-              <DeltaLogo className="mx-auto h-12 w-auto" />
+        <div className="animate-in slide-in-from-bottom-20 mx-auto grid max-w-7xl grid-cols-1 gap-6 duration-700 lg:grid-cols-3">
+          <Card className="relative overflow-hidden text-center">
+            <CardHeader className="hero-delta gap-4">
+              <DeltaLogo className="mx-auto mt-6 h-12 w-auto" />
 
               <CardDescription className="text-xl">
                 Delta is an open-source machine learning framework in Rust
@@ -41,13 +43,13 @@ export default function Hero() {
             </CardHeader>
           </Card>
 
-          <Card className="relative overflow-hidden p-0 text-center">
+          <Card className="relative overflow-hidden text-center">
             <Badge className="absolute top-3 left-3" variant="secondary">
               Coming Soon
             </Badge>
 
-            <CardHeader className="hero-nebula space-y-4 p-12">
-              <NebulaLogo className="mx-auto h-12 w-auto" />
+            <CardHeader className="hero-nebula gap-4">
+              <NebulaLogo className="mx-auto mt-6 h-12 w-auto" />
 
               <CardDescription className="text-xl">
                 A package manager for machine learning datasets and models.
@@ -61,7 +63,36 @@ export default function Hero() {
                 <Button variant="secondary" asChild>
                   <Link href="https://github.com/blackportal-ai/nebula">
                     <SiGithub />
-                    <span>Star 70+</span>
+                    <span>Star 80+</span>
+                  </Link>
+                </Button>
+              </div>
+            </CardHeader>
+          </Card>
+
+          <Card className="relative overflow-hidden text-center">
+            <Badge className="absolute top-3 left-3" variant="secondary">
+              Coming Soon
+            </Badge>
+
+            <CardHeader className="hero-horizon gap-4">
+              <div className="mx-auto mt-6 h-12 w-auto">
+                <span className="text-4xl font-bold">Horizon</span>
+              </div>
+
+              <CardDescription className="text-xl">
+                A Decentralized GPU Marketplace for Machine Learning Workloads.
+              </CardDescription>
+
+              <div className="flex items-center justify-center gap-4">
+                <Button asChild>
+                  <Link href="/horizon/docs">Read the Docs</Link>
+                </Button>
+
+                <Button variant="secondary" asChild>
+                  <Link href="https://github.com/blackportal-ai/horizon">
+                    <SiGithub />
+                    <span>Star 20+</span>
                   </Link>
                 </Button>
               </div>
